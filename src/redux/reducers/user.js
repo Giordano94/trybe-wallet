1438a7aca5463fcbaebdc129ad49779047a6e0f3
Giordano <giordano.mendes94@gmail.com>
2022-09-30 11:14:26 -0300

    config userReducer

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,19 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { GET_USER_EMAIL } from '../actions';
+
+const INITIAL_STATE_EMAIL = {
+  inputEmail: '',
+};
+
+const userEmailReducer = (state = INITIAL_STATE_EMAIL, action) => {
+  switch (action.type) {
+  case GET_USER_EMAIL:
+    return {
+      ...state,
+      inputEmail: action.userEmail,
+    };
+  default:
+    return state;
+  }
+};
+
+export default userEmailReducer;
